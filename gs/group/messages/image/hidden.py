3139c6825371fc95905b47422706d63359708596
Michael JasonSmith <mpj17@onlinegroups.net>
2011-04-14 16:53:41 +1000

    Adding the metadata to the hidden image page.

--- gs/group/messages/image/hidden.py
+++ gs/group/messages/image/hidden.py
@@ -1,6 +1,8 @@
 # coding=utf-8
+from zope.cachedescriptors.property import Lazy
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from Products.XWFFileLibrary2.hidden import FileHidden
+from metadata import Metadata
 
 class ImageHidden(FileHidden):
     index = ZopeTwoPageTemplateFile('browser/templates/imagehidden.pt')
@@ -8,4 +10,8 @@ class ImageHidden(FileHidden):
         request.form['q'] = request.URL
         request.form['f'] = request['imageId']
         FileHidden.__init__(self, context, request)
+    
+    @Lazy
+    def metadata(self):
+        return Metadata(self.context, self.fileId)
 
