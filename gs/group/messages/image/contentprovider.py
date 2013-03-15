4a14b1852f5c8adb73077a1e487278e5555e4c23
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-15 16:22:27 +1100

    Creating a new content provider for images in a group.

---
+++ gs/group/messages/image/contentprovider.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+from base64 import b64encode
+from zope.cachedescriptors.property import Lazy
+from zope.contentprovider.interfaces import UpdateNotCalled
+from zope.pagetemplate.pagetemplatefile import PageTemplateFile
+from zope.publisher.interfaces import NotFound
+from gs.group.base import GroupContentProvider
+from gs.image import GSImage
+
+
+class ImageContentProvider(GroupContentProvider):
+    """The user image."""
+
+    def __init__(self, context, request, view):
+        super(ImageContentProvider, self).__init__(context, request, view)
+        self.updated = False
+        self.pageTemplate = self.image = None
+
+    def update(self):
+        self.updated = True
+        self.pageTemplate = PageTemplateFile(self.pageTemplateFileName)
+
+        self.image = self.get_image()
+        self.resizeNeeded = self.get_resize_needed()
+        self.s = [int(d) for d in (self.width, self.height)]
+        self.shouldEmbed = self.get_should_embed()
+        self.finalSize = self.get_final_size()
+        self.finalWidth = self.finalSize[0]
+        self.finalHeight = self.finalSize[1]
+
+    def get_image(self):
+        virtualFileArchive = getattr(self.context, 'files')
+        image = virtualFileArchive.get_file_by_id(self.fileId)
+        if image is None:
+            raise NotFound(virtualFileArchive, self.fileId, self.request)
+        data = str(image)
+        retval = GSImage(data)
+        return retval
+
+    def get_resize_needed(self):
+        retval = ((self.width < self.image.width)
+                    or (self.height) < self.image.height)
+        return retval
+
+    def get_should_embed(self):
+        m1 = max(self.image.getImageSize())
+        m2 = max(self.s)
+        retval = min((m1, m2)) < 40
+        return retval
+
+    def get_final_size(self):
+        if self.resizeNeeded:
+            smallImage = self.image.get_resized(self.width, self.height)
+            retval = smallImage.getImageSize()
+        else:
+            retval = self.image.getImageSize()
+        return retval
+
+    def render(self):
+        if not self.updated:
+            raise UpdateNotCalled
+        return self.pageTemplate(view=self)
+
+    @Lazy
+    def imageUrl(self):
+        if self.shouldEmbed:
+            retval = self.embedded_image()
+        elif self.resizeNeeded:
+            retval = self.resize_link()
+        else:
+            retval = self.file_link()
+        return retval
+
+    def embedded_image(self):
+        smallImage = self.image.get_resized(self.width, self.height)
+        d = b64encode(smallImage.data)
+        r = 'data:{mediatype};base64,{data}'
+        retval = r.format(mediatype=smallImage.contentType, data=d)
+        return retval
+
+    def file_link(self):
+        r = '{group}/files/f/{fileId}'
+        retval = r.format(group=self.groupInfo.relativeURL, fileId=self.fileId)
+        return retval
+
+    def resize_link(self):
+        r = '{fileLink}/resize/{width}/{height}'
+        retval = r.format(fileLink=self.file_link(), width=self.width,
+                            height=self.height)
+        return retval
--- gs/group/messages/image/interfaces.py
+++ gs/group/messages/image/interfaces.py
@@ -1,12 +1,51 @@
 # coding=utf-8
-from zope.interface import Interface
-from zope.schema  import Int
+from zope.contentprovider.interfaces import IContentProvider
+from zope.schema import Int, ASCIILine, TextLine
 
-class IGSImageView(Interface):
-    width = Int(title=u'Width',
-        description=u'The width of the image',
-        default=432)
 
-    height = Int(title=u'Height',
-        description=u'The height of the image',
-        default=432)
+class IGSGroupImageABC(IContentProvider):
+
+    fileId = ASCIILine(title=u'File Identifier',
+                description=u'The identifier for the image.',
+                required=True)
+
+    alt = TextLine(title=u'Alt Text',
+                description=u'The alternate text for the image.',
+                required=False)
+
+
+class IGSGroupImage(IGSGroupImageABC):
+    width = Int(
+        title=u'Width',
+        description=u'The width of the image, in pixels.',
+        required=False,
+        default=50)  # FIXME: use gs.config
+
+    height = Int(
+        title=u'Height',
+        description=u'The height of the image, in pixels.',
+        required=False,
+        default=70)  # FIXME: use gs.config
+
+    pageTemplateFileName = ASCIILine(
+        title=u"Page Template File Name",
+        description=u'The name of the ZPT file that is used to render the '
+                    u'image.',
+        required=False,
+        default="browser/templates/image.pt")
+
+
+class IGSGroupImageSquare(IGSGroupImageABC):
+
+    size = Int(
+        title=u'Size',
+        description=u'The width and height of the image, in pixels.',
+        required=False,
+        default=50)  # FIXME: use gs.config
+
+    pageTemplateFileName = ASCIILine(
+        title=u"Page Template File Name",
+        description=u'The name of the ZPT file that is used to render the '
+                    u'square image.',
+        required=False,
+        default="browser/templates/squareimage.pt")
