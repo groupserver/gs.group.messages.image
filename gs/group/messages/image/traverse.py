d3e5d5a5a085a5dab77db3c8a960f3e5ce4bf228
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-15 16:21:24 +1100

    Removing some old imports

--- gs/group/messages/image/traverse.py
+++ gs/group/messages/image/traverse.py
@@ -1,35 +1,31 @@
 # coding=utf-8
-from traceback import format_exc
 from zope.component import getMultiAdapter
-from zope.location.interfaces import LocationError
-from zope.publisher.interfaces import NotFound
 from gs.group.base.page import GroupPage
 from Products.XWFFileLibrary2.error import Hidden
 from errors import NoIDError
 
 SUBSYSTEM = 'gs.group.messages.image'
 import logging
-log = logging.getLogger(SUBSYSTEM) #@UndefinedVariable
+log = logging.getLogger(SUBSYSTEM)
+
 
 class GSImageTraversal(GroupPage):
     def __init__(self, context, request):
         GroupPage.__init__(self, context, request)
-        
+
     def publishTraverse(self, request, name):
-        if request.get('imageId', None) == None:
+        if request.get('imageId', None) is None:
             request['imageId'] = name
-            
         return self
-    
+
     def __call__(self):
         try:
             retval = getMultiAdapter((self.context, self.request),
                         name="gsimage")()
-        except NoIDError, n:
+        except NoIDError, n:  # lint:ok
             retval = getMultiAdapter((self.context, self.request),
                         name="gsimage400")()
-        except Hidden, h:
+        except Hidden, h:  # lint:ok
             retval = getMultiAdapter((self.context, self.request),
                                         name="image_hidden.html")()
         return retval
-
