d34e25d582f859b1df461ee085850e73a3bf2b16
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-16 00:22:12 +1100

    Minor code cleanup.

--- gs/group/messages/image/hidden.py
+++ gs/group/messages/image/hidden.py
@@ -4,14 +4,15 @@ from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from Products.XWFFileLibrary2.hidden import FileHidden
 from metadata import Metadata
 
+
 class ImageHidden(FileHidden):
     index = ZopeTwoPageTemplateFile('browser/templates/imagehidden.pt')
+
     def __init__(self, context, request):
         request.form['q'] = request.URL
         request.form['f'] = request['imageId']
         FileHidden.__init__(self, context, request)
-    
+
     @Lazy
     def metadata(self):
         return Metadata(self.context, self.fileId)
-
