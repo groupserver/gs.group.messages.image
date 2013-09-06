98b2310beacc15a2850832f50c22b24de81a672c
Michael JasonSmith <mpj17@onlinegroups.net>
2013-09-06 11:34:46 -0700

    Adding a copyright notice to the individual files.

--- gs/group/messages/image/browser/javascript/image-min.js
+++ gs/group/messages/image/browser/javascript/image-min.js
@@ -1,3 +1,4 @@
+// Copyright (c) 2013 OnlineGroups.net and Contributors.All Rights Reserved. This software is subject to the provisions of the Zope Public License, Version 2.1 (ZPL).
 jQuery.noConflict();function gs_group_messages_image(){var a=true;jQuery(".gs-content-js-share").each(function(){a=Boolean(jQuery(this).attr("public"));
 shareWidget=GSShareBox(this,a);shareWidget.init()})}jQuery(window).load(function(){gsJsLoader.with_module("/++resource++gs-content-js-sharebox-20130305.js",gs_group_messages_image)
 });
--- gs/group/messages/image/browser/javascript/image.js
+++ gs/group/messages/image/browser/javascript/image.js
@@ -1,3 +1,14 @@
+//
+// Copyright © 2013 OnlineGroups.net and Contributors.
+// All Rights Reserved.
+//
+// This software is subject to the provisions of the Zope Public License,
+// Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+// WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+// FOR A PARTICULAR PURPOSE.
+//
 jQuery.noConflict();
 
 function gs_group_messages_image () {
