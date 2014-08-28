756c3be663e0042adb0aba7c4d34ef55a1481ee6
Michael JasonSmith <mpj17@onlinegroups.net>
2014-08-28 14:50:26 -0700

    Updating the metadata.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+############################################################################
 #
-# Copyright © 2014 OnlineGroups.net and Contributors.
+# Copyright © 2011, 2012, 2013, 2014 OnlineGroups.net and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -11,61 +11,66 @@
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
 #
-##############################################################################
+############################################################################
+import codecs
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
 
+with codecs.open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.txt"),
+                 encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
 setup(name='gs.group.messages.image',
-    version=version,
-    description="Images in a GroupServer Group",
-    long_description=open("README.txt").read() + "\n" +
-                      open(os.path.join("docs", "HISTORY.txt")).read(),
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        "Environment :: Web Environment",
-        "Framework :: Zope2",
-        "Intended Audience :: Developers",
-        'License :: OSI Approved :: Zope Public License',
-        "Natural Language :: English",
-        "Operating System :: POSIX :: Linux"
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+      version=version,
+      description="Images in a GroupServer Group",
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          "Environment :: Web Environment",
+          "Framework :: Zope2",
+          "Intended Audience :: Developers",
+          'License :: OSI Approved :: Zope Public License',
+          "Natural Language :: English",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
       ],
-    keywords='groupserver message post topic images',
-    author='Michael JasonSmith',
-    author_email='mpj17@onlinegroups.net',
-    url='http://groupserver.org/',
-    license='ZPL 2.1',
-    packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group', 'gs.group.messages'],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'setuptools',
-        'zope.browserpage',
-        'zope.browserresource',
-        'zope.cachedescriptors',
-        'zope.component',
-        'zope.contentprovider',
-        'zope.pagetemplate',
-        'zope.publisher',
-        'zope.schema',
-        'zope.tal',
-        'zope.tales',
-        'zope.viewlet',
-        'Zope2',
-        'gs.database',
-        'gs.content.layout',
-        'gs.content.js.sharebox[zope]',
-        'gs.core',
-        'gs.errormesg',
-        'gs.group.base',
-        'gs.image',
-        'Products.XWFFileLibrary2',
-    ],
-    entry_points="""
-    # -*- Entry points: -*-
+      keywords='groupserver, group, message, post, topic, images',
+      author='Michael JasonSmith',
+      author_email='mpj17@onlinegroups.net',
+      url='https://source.iopen.net/groupserver/gs.group.messages.image/',
+      license='ZPL 2.1',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['gs', 'gs.group', 'gs.group.messages'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.browserpage',
+          'zope.browserresource',
+          'zope.cachedescriptors',
+          'zope.component',
+          'zope.contentprovider',
+          'zope.pagetemplate',
+          'zope.publisher',
+          'zope.schema',
+          'zope.tal',
+          'zope.tales',
+          'zope.viewlet',
+          'Zope2',
+          'gs.database',
+          'gs.content.layout',
+          'gs.content.js.sharebox[zope]',
+          'gs.core',
+          'gs.errormesg',
+          'gs.group.base',
+          'gs.image',
+          'Products.XWFFileLibrary2',
+      ],
+      entry_points="""# -*- Entry points: -*-
     """,)
