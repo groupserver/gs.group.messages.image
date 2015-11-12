495371a1801eeb421f667a83275968e8d61905db
Michael JasonSmith <mpj17@onlinegroups.net>
2015-11-12 13:21:11 -0800

    Minifying

--- gs/group/messages/image/browser/javascript/image-min.js
+++ gs/group/messages/image/browser/javascript/image-min.js
@@ -1,4 +1,1 @@
-"use strict";jQuery.noConflict();function gs_group_messages_image(){var b=true,a=null;
-jQuery(".gs-content-js-share").each(function(){b=Boolean(jQuery(this).attr("public"));
-a=GSShareBox(this,b);a.init()})}jQuery(window).load(function(){gsJsLoader.with_module("/++resource++gs-content-js-sharebox-min-20140327.js",gs_group_messages_image)
-});
+'use strict';jQuery.noConflict();function gs_group_messages_image(){var a=!0,b=null;jQuery(".gs-content-js-share").each(function(){a=Boolean(jQuery(this).attr("public"));b=GSShareBox(this,a);b.init()})}jQuery(window).load(function(){gsJsLoader.with_module("/++resource++gs-content-js-sharebox-min-20151112.js",gs_group_messages_image)});
